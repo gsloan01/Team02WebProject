fc02f16a27f697af77a0fca91b1f43f25b940b97
Leonardo Contreras <LContreras@student.neumont.edu>
2021-02-19 17:03:03 -0800

    Database Base succesfully connected. Registration Page is Set Up and successfully uploads the data onto the database

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 using System.Web.Mvc;
+using MTGDeckBuilder.Models;
+using System.Security.Cryptography;
 
 namespace MTGDeckBuilder.Controllers
 {
     public class HomeController : Controller
     {
+        private DB_Entities db = new DB_Entities();
+
         public ActionResult Index()
         {
             return View();
@@ -47,11 +52,56 @@ namespace MTGDeckBuilder.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var check = db.Users.FirstOrDefault(s => s.Username == user.Username);
+                if (check == null)
+                {
+                    user.Password = GetMD5(user.Password);
+                    db.Configuration.ValidateOnSaveEnabled = false;
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Username Already Exit";
+                    return View();
+                }
+
+
+            }
+            return View();
+
+
+        }
+
+
         public ActionResult LogIn()
         {
             ViewBag.Message = "Log in.";
 
             return View();
         }
+
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
     }
 }
