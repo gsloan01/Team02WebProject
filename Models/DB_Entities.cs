c1a1af8f65ce78e8351356710d53eeb604d70565
Leonardo Contreras <LContreras@student.neumont.edu>
2021-02-24 16:58:00 -0800

    Fixes to DB

--- Models/DB_Entities.cs
+++ Models/DB_Entities.cs
@@ -5,11 +5,12 @@ using System.Web;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 
+
 namespace MTGDeckBuilder.Models
 {
     public class DB_Entities : DbContext
     {
-        public DB_Entities() : base("DB_A6FB48_MTGDeckBuilderDBEntities") { }
+        public DB_Entities() : base("DB_A6FB48_MTGDeckBuilderDBEntities2") { }
         public DbSet<User> Users { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
--- Models/MtgADO.Context.cs
+++ Models/MtgADO.Context.cs
@@ -13,10 +13,10 @@ namespace MTGDeckBuilder.Models
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     
-    public partial class DB_A6FB48_MTGDeckBuilderDBEntities : DbContext
+    public partial class DB_A6FB48_MTGDeckBuilderDBEntities2 : DbContext
     {
-        public DB_A6FB48_MTGDeckBuilderDBEntities()
-            : base("name=DB_A6FB48_MTGDeckBuilderDBEntities")
+        public DB_A6FB48_MTGDeckBuilderDBEntities2()
+            : base("name=DB_A6FB48_MTGDeckBuilderDBEntities2")
         {
         }
     
