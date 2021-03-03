54ccaae674c7eb721f86687caaeadc39a13b23fc
Leonardo Contreras <LContreras@student.neumont.edu>
2021-03-02 17:04:13 -0800

    Clean Up Repo

--- Models/DeckRepo.cs
+++ Models/DeckRepo.cs
@@ -21,43 +21,26 @@ namespace MTGDeckBuilder.Models
                 System.Data.Entity.Core.EntityClient.EntityConnectionStringBuilder efBuilder = new System.Data.Entity.Core.EntityClient.EntityConnectionStringBuilder(_connString);
                 _connString = efBuilder.ProviderConnectionString;
             }
-            //
-            // Create a connection to the DB
+
             using (var con = new SqlConnection(_connString))
             {
-                //
-                // Set-up your SQL command
                 var cmd = new SqlCommand("GetDecks", con);
                 cmd.CommandType = CommandType.StoredProcedure;
 
                 cmd.Parameters.AddWithValue("@playerId", id);
-                //cmd.Parameters.AddWithValue("@eMail", eMail);
-                //cmd.Parameters.AddWithValue("@birthDate", birthDate);
-                //cmd.Parameters.AddWithValue("@showInActives", showInactive);
 
-                //
-                // Open your connection
                 con.Open();
-                //
-                // Create your SQL Data Reader to execute your command and get your data
+
                 SqlDataReader rdr = cmd.ExecuteReader();
-                //
-                // Loop through all
+
                 while (rdr.Read())
                 {
-                    //
-                    // Create your Student object
                     var deck = new DeckTB();
-                    //
-                    // Get all the values from your reader loaded into your object
+
                     deck.DeckId = Convert.ToInt32(rdr["DeckId"]);
                     deck.PlayerId = Convert.ToInt32(rdr["PlayerId"]);
                     deck.DeckName = rdr["DeckName"].ToString();
-                    //stu.Birthdate = Convert.ToDateTime(rdr["BirthDate"]);
-                    //stu.EMail = rdr["EMail"].ToString();
-                    //stu.IsActive = Convert.ToBoolean(rdr["IsActive"]);
-                    //
-                    // Add your object to your list
+
                     filteredList.Add(deck);
                 }
             }
