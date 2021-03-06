3a464c0a8c80c28f7870c301c83610c74c693e65
gsloan01 <60530127+gsloan01@users.noreply.github.com>
2021-03-05 18:53:18 -0700

    Set up the about page, we also have tons of cards

--- Controllers/CustomCardTBsController.cs
+++ Controllers/CustomCardTBsController.cs
@@ -69,6 +69,7 @@ namespace MTGDeckBuilder.Controllers
             if (ModelState.IsValid)
             {
                 customCardTB.PlayerId = Int32.Parse(Session["Id"].ToString());
+
                 db.CustomCardTBs.Add(customCardTB);
                 db.SaveChanges();
                 return RedirectToAction("YourCards");
